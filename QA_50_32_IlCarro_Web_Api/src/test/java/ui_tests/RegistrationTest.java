a59ed050603e2cb03924f3d87b2bd569fef1aace
IlyaQaTest <lapidusqa@gmail.com>
2026-01-30 09:57:48 +0200

    Registration negative test

--- QA_50_32_IlCarro_Web_Api/src/test/java/ui_tests/RegistrationTest.java
+++ QA_50_32_IlCarro_Web_Api/src/test/java/ui_tests/RegistrationTest.java
@@ -5,6 +5,7 @@ import manager.ApplicationManager;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.LoginPage;
 import pages.PopUpPage;
 import pages.RegistrationPage;
@@ -17,6 +18,7 @@ import java.util.Random;
 
 public class RegistrationTest extends ApplicationManager {
     RegistrationPage registrationPage;
+    SoftAssert softAssert = new SoftAssert();
 
     @BeforeMethod
     public void goToRegistrationPage() {
@@ -48,5 +50,58 @@ public class RegistrationTest extends ApplicationManager {
         registrationPage.clickBtnYalla();
         Assert.assertTrue(new PopUpPage(getDriver())
                 .isTextInPopUpMessagePresent("You are logged in success"));
+    }
+
+    @Test
+    public void registrationNegativeTest_UserAlreadyExist() {
+        User user = User.builder()
+                .firstname("qwerty")
+                .lastname("qwerty")
+                .email("family@mail.ru")
+                .password("Family123!")
+                .build();
+        registrationPage.typeRegistrationForm(user);
+        registrationPage.setCheckBoxAgreeTermsOfUse();
+        registrationPage.clickBtnYalla();
+        Assert.assertTrue(new PopUpPage(getDriver())
+                .isTextInPopUpMessagePresent("User already exists"));
+    }
+
+    @Test
+    public void registrationNegativeTest_WithSpaceInFirstName() {
+        User user = User.builder()
+                .firstname(" ")
+                .lastname("qwerty")
+                .email("family@mail.ru")
+                .password("Family123!")
+                .build();
+        registrationPage.typeRegistrationForm(user);
+        registrationPage.setCheckBoxAgreeTermsOfUse();
+        registrationPage.clickBtnYalla();
+        Assert.assertTrue(new PopUpPage(getDriver())
+                .isTextInPopUpMessagePresent("must not be blank"));
+    }
+
+    @Test
+    public void registrationNegativeTest_WithAllEmptyFields() {
+        User user = User.builder()
+                .firstname("")
+                .lastname("")
+                .email("")
+                .password("")
+                .build();
+        registrationPage.typeRegistrationForm(user);
+        registrationPage.setCheckBoxAgreeTermsOfUse();
+        registrationPage.clickBtnYalla();
+        softAssert.assertTrue(registrationPage.isTextInError("Name is required")
+                ,"validate error message Name is required");
+        softAssert.assertTrue(registrationPage.isTextInError("Last name is required")
+                ,"validate error message Last Name is required");
+        softAssert.assertTrue(registrationPage.isTextInError("Email is required")
+                ,"validate error message Email is required");
+        softAssert.assertTrue(registrationPage.isTextInError("Password is required")
+                ,"validate error message Password is required");
+        softAssert.assertAll();
+
     }
 }
